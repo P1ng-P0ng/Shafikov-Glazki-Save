c90448b2e3cc77345a378c4aed626c786aa514f3
p1ng-p0ng <studygit@mail.ru>
2023-11-08 23:10:43 +0500

    lab11

--- Shafikov-Glazki-Save/GlazkiPage.xaml.cs
+++ Shafikov-Glazki-Save/GlazkiPage.xaml.cs
@@ -23,11 +23,99 @@ namespace Shafikov_Glazki_Save
         public GlazkiPage()
         {
             InitializeComponent();
+
+            var currentGlazki = Shafikov_GlazkiEntities.GetContext().Agent.ToList();
+            GlazkiListView.ItemsSource = currentGlazki;
+
+            ComboSort.SelectedIndex = 0;
+            ComboType.SelectedIndex = 0;
+
+            UpdateGlazki();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void UpdateGlazki()
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            var currentGlazki = Shafikov_GlazkiEntities.GetContext().Agent.ToList();
+
+            currentGlazki = currentGlazki.Where(p => (p.Title.ToLower().Contains(TBoxSearch.Text.ToLower()) 
+            || p.Phone.Replace("+","").Replace(" ", "").Replace("(", "").Replace(")", "").Replace("-", "").Contains(TBoxSearch.Text)
+            || p.Email.ToLower().Contains(TBoxSearch.Text.ToLower()))).ToList();
+
+            if (ComboSort.SelectedIndex == 1) 
+            { 
+                currentGlazki = currentGlazki.OrderBy(p => p.Title).ToList();
+            }
+            if(ComboSort.SelectedIndex == 2)
+            {
+                currentGlazki = currentGlazki.OrderByDescending(p => p.Title).ToList();
+            }
+            if (ComboSort.SelectedIndex == 3)
+            {
+                
+            }
+            if (ComboSort.SelectedIndex == 4)
+            {
+                
+            }
+            if (ComboSort.SelectedIndex == 5)
+            {
+                currentGlazki = currentGlazki.OrderBy(p => p.Priority).ToList();
+            }
+            if (ComboSort.SelectedIndex == 6)
+            {
+                currentGlazki = currentGlazki.OrderByDescending(p => p.Priority).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 0)
+            {
+
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentGlazki = currentGlazki.Where(p => p.AgentTypeString == "МФО").ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentGlazki = currentGlazki.Where(p => p.AgentTypeString == "ООО").ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentGlazki = currentGlazki.Where(p => p.AgentTypeString == "ЗАО").ToList();
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentGlazki = currentGlazki.Where(p => p.AgentTypeString == "МКК").ToList();
+            }
+            if (ComboType.SelectedIndex == 5)
+            {
+                currentGlazki = currentGlazki.Where(p => p.AgentTypeString == "ОАО").ToList();
+            }
+            if (ComboType.SelectedIndex == 6)
+            {
+                currentGlazki = currentGlazki.Where(p => p.AgentTypeString == "ПАО").ToList();
+            }
+
+            GlazkiListView.ItemsSource = currentGlazki;
         }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateGlazki();
+        }
+
+        private void ComboSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateGlazki();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateGlazki();
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Manager.MainFrame.Navigate(new AddEditPage());
+        //}
     }
 }
